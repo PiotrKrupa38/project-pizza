3c51734a8bb8ee4e4d8b7d46eda7aa9ca33c3259
Piotr Krupa <piotrkrupa.email@gmail.com>
2023-07-30 15:55:39 +0200

    Adding and removing images

--- src/js/script.js
+++ src/js/script.js
@@ -88,6 +88,7 @@ const select = {
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -156,10 +157,11 @@ const select = {
   for(let optionId in param.options) {
   // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
   const option = param.options[optionId];
-  console.log(optionId, option);
+  const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
 
   // check if there is param with a name of paramId in formData and if it includes optionId
-  if(formData[paramId] && formData[paramId].includes(optionId)) {
+  //if(formData[paramId] && formData[paramId].includes(optionId)) {
+    if (optionSelected) {
     // check if the option is not default
     if(!option.default == true) {
       // add option price to price variable
@@ -172,7 +174,15 @@ const select = {
       price -= option.price
     }
   }
-
+  const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+  if (optionImage) {
+    if (optionSelected) {
+      optionImage.classList.add(classNames.menuProduct.imageVisible);
+    }
+    else {
+      optionImage.classList.remove(classNames.menuProduct.imageVisible);
+    }
+  }
 }
   }
 
