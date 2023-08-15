79c46dc7ab359bc3e5d1d6566359aebe3e012c2a
Piotr Krupa <piotrkrupa.email@gmail.com>
2023-08-15 20:40:01 +0200

    Adding pages functions - 9.4

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,11 +2,16 @@ export const select = {
     templateOf: {
       menuProduct: "#template-menu-product",
       cartProduct: '#template-cart-product',
+      bookingWidget: '#template-booking-widget',
+
       },
       containerOf: {
         menu: '#product-list',
         cart: '#cart',
+        pages: '#pages',
+        booking: '.booking-wrapper',
       },
+
       all: {
         menuProducts: '#product-list > .product',
         menuProductsActive: '#product-list > .product.active',
@@ -26,7 +31,24 @@ export const select = {
           linkDecrease: 'a[href="#less"]',
           linkIncrease: 'a[href="#more"]',
         },
+        datePicker: {
+            wrapper: '.date-picker',
+            input: `input[name="date"]`,
+        },
+        hourPicker: {
+            wrapper: '.hour-picker',
+            input: 'input[type="range"]',
+            output: '.output',
+        },
       },
+      booking: {
+        peopleAmount: '.people-amount',
+        hoursAmount: '.hours-amount',
+        tables: '.floor-plan .table',
+    },
+        nav: {
+        links: '.main-nav a',
+    },
   
        // CODE ADDED START
     cart: {
@@ -56,11 +78,21 @@ export const classNames = {
         imageVisible: 'active',
       },
   
-      // CODE ADDED START
+      
     cart: {
       wrapperActive: 'active',
     },
-    // CODE ADDED END
+    booking: {
+        loading: 'loading',
+        tableBooked: 'booked',
+    },
+    nav: {
+        active: 'active',
+    },
+    pages: {
+        active: 'active',
+    }
+    
     };
   
 export const settings = {
@@ -73,17 +105,36 @@ export const settings = {
     cart: {
       defaultDeliveryFee: 20,
     },
+    hours: {
+        open: 12,
+        close: 24,
+    },
+    datePicker: {
+        maxDaysInFuture: 14,
+    },
+    booking: {
+        tableIdAttribute: 'data-table',
+    },
     // CODE ADDED END
     db: {
       url: '//localhost:3131',
       products: 'products',
       orders: 'orders',
+      product: 'product',
+      order: 'order',
+      booking: 'booking',
+      event: 'event',
+      dateStartParamKey: 'date_gte',
+      dateEndParamKey: 'date_lte',
+      notRepeatParam: 'repeat=false',
+      repeatParam: 'repeat_ne=false',
     },
     };
   
 export const templates = {
       menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
       // CODE ADDED START
-    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+      cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
     // CODE ADDED END
+      bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
     };
